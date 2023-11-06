839826800c552a94171f8197363b5ebce23ef144
Reagan <reaganrandolph1015@gmail.com>
2023-11-06 08:44:33 -0600

    Working on multi-round functionality, stuck on returning scores

--- index.js
+++ index.js
@@ -10,16 +10,44 @@ function getComputerChoice(){
 function playRound(computerChoice, playerChoice){
     if (computerChoice == playerChoice) {
         return "Draw!"
-    } else if (computerChoice == 'scissors' && playerChoice == 'rock'){
-        return "You win!"
-    } else if (computerChoice == 'paper' && playerChoice == 'rock'){
+    } else if (computerChoice === 'scissors' && playerChoice === 'paper'){
         return "You lose!"
-    } 
+    } else if (computerChoice === 'scissors' && playerChoice === 'rock'){
+        return "You win!"
+    } else if (computerChoice === 'paper' && playerChoice === 'rock'){
+        return "You lose!"
+    } else if (computerChoice === 'paper' && playerChoice === 'scissors'){
+        return "You win!"
+    } else if (computerChoice === 'rock' && playerChoice === 'scissors'){
+        return "You lose!"
+    } else if (computerChoice === 'rock' && playerChoice === 'paper'){
+        return "You win!"
+    }
 }
 
-let computerChoice = getComputerChoice()
+const computerChoice = getComputerChoice()
 let playerChoice = "rock"
 
-console.log(computerChoice) //show NPC choice
+//console.log(computerChoice) //show NPC choice
 
-console.log(playRound(computerChoice,playerChoice))
+//console.log(playRound(computerChoice,playerChoice)) //1 round functionality
+
+function game() {
+    let playerScore = 0
+    let computerScore = 0
+    let playerChoice = prompt("Rock, paper, scissors?")
+    console.log(playerScore, computerScore)
+    console.log(computerChoice)
+    playRound(computerChoice, playerChoice)
+    
+    
+    if (playRound() === "You win!") {
+        playerScore++
+    } else if (playRound() === "You lose!") {
+        computerScore++
+    }
+    console.log(playRound(computerChoice, playerChoice))
+    return(playerScore, computerScore)
+} 
+
+console.log(game())
